736351a2421e6e033f169971277df87c74e11b5b
randyrafaelcg <Randy.Cardoso@fundacion-jala.org>
2022-02-12 16:02:01 -0400

    Change player function implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const tiles = Array.from(document.querySelectorAll(".tile"));
 
-tiles.forEach((tile) => {
+var currentPlayer = "X";
+tiles.forEach((tile, index) => {
   tile.addEventListener("click", () => userAction(tile));
 });
 
 function userAction(tile) {
-  tile.innerText = "X";
+  if(tile.innerText!="X" && tile.innerText!="O"){
+  tile.innerText = currentPlayer;
+  if (currentPlayer == "X") currentPlayer = "O";
+  else currentPlayer = "X";
+  }
 }
